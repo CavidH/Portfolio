416bfd9a4a0df3fc0403d5e10f9079707680ed97
Cavid <hcavid386@gmail.com>
2022-12-09 06:08:08 -0800

    dbcontext

---
+++ Portfolio.Data/DAL/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Portfolio.Core.Entities;
+
+namespace Portfolio.Data.DAL
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Skill> Skills { get; set; }
+    }
+}
